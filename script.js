9e5627da51f4b9bbbe2cf2b09b21e4a00037170a
Claaaaaarity <egor20019@gmail.com>
2021-12-01 23:19:56 +0300

    lab7

---
+++ script.js
@@ -0,0 +1,12 @@
+function scrollToElement3(e) {
+    element = document.getElementById("myElementToScroll3")
+    element.scrollIntoView({ behavior: "smooth" });
+}
+function scrollToElement2(e) {
+    element = document.getElementById("myElementToScroll2")
+    element.scrollIntoView({ behavior: "smooth" });
+}
+function scrollToElement1(e) {
+    element = document.getElementById("myElementToScroll1")
+    element.scrollIntoView({ behavior: "smooth" });
+}
